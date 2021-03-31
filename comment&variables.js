2c4b13e669309912a3bef390c3a2a1cf7b1c476a
Park Byeong Jin <67573062+fatejin@users.noreply.github.com>
2021-03-31 14:29:48 +0900

    2021-03-31-comment&var_js
    I practice and elaborate of the Jscode

---
+++ comment&variables.js
@@ -0,0 +1,32 @@
+// This is a single line comment
+
+/*
+This is a 
+multi-line 
+comment
+*/
+
+/********  This is a sample comment *******/
+
+// console.log('Hello') ctrl + / = //
+
+// console.log('Hello') everything select, ctrl + / = // 
+// dasdasdas
+// asdsadsa
+// dasdasd
+
+
+/********* Variables *********/
+
+let x; 
+x = 10;
+let x = 10;
+
+let message = 'Hello';
+message = 'Welcome';
+
+let x,y;
+x=10;
+y=20;
+
+let x=10, y=20;
